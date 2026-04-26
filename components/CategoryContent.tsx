7ab25eab847a996dc612562ef39b2791d2175035
saulvo <vtson23597@gmail.com>
2026-04-26 22:22:01 +0700

    fix: remove invalid categories prop from PostList.Provider

--- components/CategoryContent.tsx
+++ components/CategoryContent.tsx
@@ -10,7 +10,7 @@ interface CategoryContentProps {
 
 export default function CategoryContent({ posts, category }: CategoryContentProps) {
   return (
-    <PostList.Provider allPosts={posts} categories={[]} initialCategory={category}>
+    <PostList.Provider allPosts={posts} initialCategory={category}>
       <PostList.Grid />
       <PostList.LoadMore />
     </PostList.Provider>
